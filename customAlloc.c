6dbb297884ed5c6e0c8fe4a2501e053300c4269f
Elijah Frost <harshgaur563@gmail.com>
2024-01-16 12:50:33 +0530

    Major improvement in heap allocation and possibly faster lookup times

--- customAlloc.c
+++ customAlloc.c
@@ -23,12 +23,20 @@ typedef struct{
 }Chunk_list;
 
 char heap[HEAP_ALLOC_CAP] = {0};
-size_t used = 0;
 
-Chunk_list freed_chunks[CHUNK_LIST_CAP ] = {0};
+Chunk_list freed_chunks[CHUNK_LIST_CAP] =  
+{
+    {
+    .count = 1,
+    .chunks ={ [0] = {.ptr = heap, .size = sizeof(heap)}}
+
+    }
+};
+
 Chunk_list alloced_chunks[CHUNK_LIST_CAP ] = {0};
 
 void heap_dump_alloced_chunk(Chunk_list * list){
+    
     printf("Status report: \nNO. of allocation %ld, address of first allocation\t%p\n", list->count, list->chunks[0].ptr);
     for (size_t i = 0; i < list->count; i++)
     {printf("Start: %p, size: %zu\n", 
@@ -42,9 +50,9 @@ void *show_current_index(){
     UNIMPLEMENTED;
 }
 
-int Chunk_insert(Chunk_list *list, void *start, size_t size){
+void Chunk_insert(Chunk_list *list, void *start, size_t size){
 
-    assert(size < CHUNK_LIST_CAP);
+   
     list->chunks[list->count].ptr = start;
     list->chunks[list->count].size= size;
 
@@ -56,27 +64,8 @@ int Chunk_insert(Chunk_list *list, void *start, size_t size){
     }
 
     list->count++;  
-
-    return list->count;
 }
  
-
-void *heap_alloc(size_t requestSize){    
-
-    if(requestSize > 0)
-    {
-        assert(used + requestSize < HEAP_ALLOC_CAP);
-        void *  ptr = heap + used;
-        used += requestSize;
-        Chunk_insert(alloced_chunks, ptr, requestSize);
-        return ptr;
-    }
-    else{
-        return NULL;
-    }
-}
-
-
 void Chunk_remove(Chunk_list *list, size_t index){
     assert(index < list->count);
     for (size_t i = index; i < list->count - 1; i++)
@@ -84,6 +73,31 @@ void Chunk_remove(Chunk_list *list, size_t index){
         list->chunks[i] = list->chunks[i+1];
     }
     list->count--;
+}
+
+void *heap_alloc(size_t requestSize){    
+    if(requestSize > 0){
+        for (size_t i = 0; i < freed_chunks->count; i++)
+        {
+        const Chunk chunk = freed_chunks->chunks[i];
+        const Chunk alChunk = alloced_chunks->chunks[i];
+            if(chunk.size >= requestSize){
+                Chunk_remove(freed_chunks, i);
+                Chunk_insert(alloced_chunks, chunk.ptr, requestSize);
+
+                const size_t tail_size = chunk.size - requestSize;
+                if(tail_size >= 0)
+                    Chunk_insert(freed_chunks, chunk.ptr + requestSize, tail_size);
+                return chunk.ptr;
+            }
+
+        }
+
+
+    }
+    
+    
+    return NULL;
 }
 
 int chunk_compar(const void *a, const void *b){
@@ -103,8 +117,6 @@ int Chunk_find(Chunk_list *list, void *ptr){
     Chunk *desire = bsearch(&key, list->chunks,
             list->count, sizeof(list->chunks[0]),
             chunk_compar);
-    
-    printf("%d\n: %s", desire, __func__);
 
     if(desire != 0){
         assert(desire > 0);
@@ -119,12 +131,10 @@ void free_mem(void *heap){
 
     if(heap != NULL){
         const int index = Chunk_find(alloced_chunks, heap);
-
-        printf("%d\n: %s", index, __func__);
-
         assert(index >= 0);
-        Chunk_remove(alloced_chunks, (size_t) index);
         Chunk_insert(freed_chunks, alloced_chunks->chunks[index].ptr, alloced_chunks->chunks[index].size);
+        Chunk_remove(alloced_chunks, (size_t) index);
+ 
     }
 }
 
@@ -137,11 +147,19 @@ int main(){
     for (size_t i = 0; i < 10; i++)
     {
         void *p = heap_alloc(i);
-        if(i%2 == 0){
-            printf(free_mem(i));
+        if(i%2 == 0){ 
+            free_mem(p);
         }
     }
+
+    heap_alloc(420);
+
+    for (size_t i = 1; i <= 4; i++)
+    {
+        heap_alloc(i);
+    }
     
+
     heap_dump_alloced_chunk(alloced_chunks);
     heap_dump_alloced_chunk(freed_chunks);
 
